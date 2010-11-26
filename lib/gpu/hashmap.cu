fb7590966c7a4979d175623cb9fa79a9239b5c57
Nathan Chong <nyc04@doc.ic.ac.uk>
2010-11-26 22:37:02 +0000

    Correct bad out-by-one error. Also harden up cuda calls so we crash appropriately as soon as something bad happens

--- lib/gpu/hashmap.cu
+++ lib/gpu/hashmap.cu
@@ -1,3 +1,17 @@
+
+#ifndef CUDA_HASHMAP
+#define CUDA_HASHMAP
+
+#define ASSERT_NO_CUDA_ERROR( callReturningErrorstatus ) {     \
+  cudaError_t err = callReturningErrorstatus;                  \
+  if (err != cudaSuccess) {                                    \
+    fprintf(stderr,                                            \
+            "Cuda error (%s/%d) in file '%s' in line %i\n",    \
+            cudaGetErrorString(err), err, __FILE__, __LINE__); \
+    exit(1);                                                   \
+  }                                                            \
+} while(0);
+
 #include <stdio.h>
 #include <assert.h>
 
@@ -20,8 +34,16 @@ struct hashmap {
 // --------------------------------------------------------------------------
 struct hashmap *create_hashmap(int size) {
   struct hashmap *hm = (struct hashmap *) malloc(sizeof(struct hashmap));
+  if (!hm) {
+    printf("error: could not malloc hashmap struct\n");
+    exit(1);
+  }
   hm->size = size;
   hm->map = (struct entry *) malloc(sizeof(struct entry) * size);
+  if (!hm->map) {
+    printf("error: could not malloc map of size %d\n", size);
+    exit(1);
+  }
   for (int i=0; i<size; i++) {
     hm->map[i].valid = false;
     hm->map[i].key = 0;
@@ -29,26 +51,42 @@ struct hashmap *create_hashmap(int size) {
     hm->map[i].shear[1] = 0;
     hm->map[i].shear[2] = 0;
   }
+  assert(hm);
+  assert(hm->size == size);
+  assert(hm->map);
   return hm;
 }
 
-void destroy_hashmap(struct hashmap *hm) {
+void free_hashmap(struct hashmap *hm) {
   free(hm->map);
   free(hm);
 }
 
+void free_device_hashmap(struct hashmap *d_hm) {
+  struct entry *d_map;
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(&d_map, &d_hm->map, sizeof(&d_map), cudaMemcpyDeviceToHost));
+  ASSERT_NO_CUDA_ERROR(
+    cudaFree(d_map)
+  );
+  ASSERT_NO_CUDA_ERROR(
+    cudaFree(d_hm)
+  );
+}
+
 struct entry *insert_hashmap(struct hashmap *hm, int key, double shear[3]) {
+  assert(hm->size > 0);
   int hash = key % (hm->size);
 
   //linear probe on collision
   if (hm->map[hash].valid) {
-    int probe = hash + 1;
+    int probe = (hash + 1) % hm->size;
     while (hm->map[probe].valid && probe != hash) {
       probe = (probe + 1) % hm->size;
     }
     
     if (probe == hash) {
-      printf("hm is full!\n"); //could resize here
+      printf("error: hashmap is full!\n"); //could resize here
       exit(1);
     } else {
       hash = probe;
@@ -65,12 +103,16 @@ struct entry *insert_hashmap(struct hashmap *hm, int key, double shear[3]) {
 }
 
 struct entry *retrieve_hashmap(struct hashmap *hm, int key) {
+  assert(hm);
+  assert(hm->map);
+  assert(hm->size > 0 );
+
   struct entry *result = NULL;
   int hash = key % (hm->size);
   if (hm->map[hash].valid && hm->map[hash].key == key) {
     result = &hm->map[hash];
   } else {
-    int probe = hash + 1;
+    int probe = (hash + 1) % hm->size;
     while (hm->map[probe].key != key && probe != hash) {
       probe = (probe + 1) % hm->size;
     }
@@ -103,20 +145,20 @@ void print_hashmap(struct hashmap *hm) {
 struct hashmap *c_to_device_hashmap(struct hashmap *hm) {
   struct entry *d_map;
   size_t table_size = sizeof(struct entry) * hm->size;
-  cudaMalloc((void **)&d_map, table_size);
-  cudaMemcpy(d_map, hm->map, table_size, cudaMemcpyHostToDevice);
+  ASSERT_NO_CUDA_ERROR(
+    cudaMalloc((void **)&d_map, table_size));
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(d_map, hm->map, table_size, cudaMemcpyHostToDevice));
 
   struct hashmap *d_hm;
-  cudaMalloc((void **)&d_hm, sizeof(struct hashmap));
-  cudaMemcpy(&d_hm->size, &hm->size, sizeof(int), cudaMemcpyHostToDevice);
-  //nb copying sizeof pointer *not* the table here
-  cudaMemcpy(&d_hm->map, &d_map, sizeof(&d_map), cudaMemcpyHostToDevice);
+  ASSERT_NO_CUDA_ERROR(
+    cudaMalloc((void **)&d_hm, sizeof(struct hashmap)));
 
-  cudaError_t err = cudaGetLastError();
-  if (err != cudaSuccess) {
-    printf("create_device_hashmap_of: %s.\n", cudaGetErrorString(err));
-    exit(1);
-  }
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(&d_hm->size, &hm->size, sizeof(int), cudaMemcpyHostToDevice));
+  //nb copying sizeof pointer *not* the table here
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(&d_hm->map, &d_map, sizeof(&d_map), cudaMemcpyHostToDevice));
 
   return d_hm;
 }
@@ -126,15 +168,18 @@ struct hashmap *c_to_device_hashmap(struct hashmap *hm) {
  */
 struct hashmap *device_to_c_hashmap(struct hashmap *d_hm) {
   struct hashmap *hm = (struct hashmap *) malloc(sizeof(struct hashmap));
-  cudaMemcpy(&hm->size, &d_hm->size, sizeof(int), cudaMemcpyDeviceToHost);
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(&hm->size, &d_hm->size, sizeof(int), cudaMemcpyDeviceToHost));
 
   //nb this is a *device* pointer to unwrap the hashmap struct
   //doing this directly leads to cudaError#11 (Invalid argument)
   struct entry *d_map;
-  cudaMemcpy(&d_map, &d_hm->map, sizeof(d_map), cudaMemcpyDeviceToHost);
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(&d_map, &d_hm->map, sizeof(d_map), cudaMemcpyDeviceToHost));
   size_t table_size = sizeof(struct entry) * hm->size;
   struct entry *map = (struct entry *) malloc(table_size);
-  cudaMemcpy(map, d_map, table_size, cudaMemcpyDeviceToHost);
+  ASSERT_NO_CUDA_ERROR(
+    cudaMemcpy(map, d_map, table_size, cudaMemcpyDeviceToHost));
   hm->map = map;
 
   return hm;
@@ -156,7 +201,7 @@ __device__ struct entry *cuda_retrieve_hashmap(struct hashmap *hm, int key) {
   if (hm->map[hash].valid && hm->map[hash].key == key) {
     result = &hm->map[hash];
   } else {
-    int probe = hash + 1;
+    int probe = (hash + 1) % hm->size;
     while (hm->map[probe].key != key && probe != hash) {
       probe = (probe + 1) % hm->size;
     }
@@ -169,3 +214,5 @@ __device__ struct entry *cuda_retrieve_hashmap(struct hashmap *hm, int key) {
 }
 
 // no insertion on device
+
+#endif
