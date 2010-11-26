44570ada47a3382c2a6cfa9f52de3edd1732800a
Nathan Chong <nyc04@doc.ic.ac.uk>
2010-11-26 22:42:53 +0000

    Kernel updated using play/hertz.cu; outer kernel host code in hertz_gpu.cu does appropriate setup including shearmap (array of shear-history hashmaps datastructure)

--- lib/gpu/hertz_gpu.cu
+++ lib/gpu/hertz_gpu.cu
@@ -5,7 +5,7 @@
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under 
+   certain rights in this software.  This software is distributed under
    the GNU General Public License.
 
    See the README file in the top-level LAMMPS directory.
@@ -27,6 +27,8 @@
 #include "lj_gpu_memory.cu"
 #include "hertz_gpu_kernel.h"
 
+#include "hashmap.cu"
+
 // ---------------------------------------------------------------------------
 // Return string with GPU info
 // ---------------------------------------------------------------------------
@@ -38,7 +40,7 @@ EXTERN void hertz_gpu_name(const int id, const int max_nbors, char * name) {
 // Allocate memory on host and device and copy constants to device
 // ---------------------------------------------------------------------------
 EXTERN bool hertz_gpu_init(
-  double *boxlo, double *boxhi, 
+  double *boxlo, double *boxhi,
 	double cell_size, double skin)
 {
 
@@ -48,12 +50,12 @@ EXTERN bool hertz_gpu_init(
 
   printf("> hertz_gpu_init\n");
   printf("> cell_size = %f\n", cell_size);
-  printf("> boxhi = {%f, %f, %f}; boxlo = {%f, %f, %f}\n", 
+  printf("> boxhi = {%f, %f, %f}; boxlo = {%f, %f, %f}\n",
     boxhi[0], boxhi[1], boxhi[2],
     boxlo[0], boxlo[1], boxlo[2]);
   printf("> ncellx = %d; ncelly = %d; ncellz = %d;\n",
     ncellx, ncelly, ncellz);
-   
+
   init_cell_list_const(cell_size, skin, boxlo, boxhi);
 
   return true;
@@ -65,14 +67,193 @@ EXTERN bool hertz_gpu_init(
 EXTERN void hertz_gpu_clear() {
 }
 
-template <class numtyp, class acctyp>
-double _hertz_gpu_cell()
-{
-  return 0.0;
+EXTERN struct hashmap **create_shearmap(
+  //number of atoms (ranged over by ii)
+  int inum, /*list->inum*/
+  //id of atom ii (ranged over by i)
+  int *ilist, /*list->ilist*/
+  //numneigh[i] is the number of neighbor atoms to atom i (ranged over by jj)
+  int *numneigh, /*list->numneigh*/
+  //firstneigh[i][jj] is the id of atom jj (ranged over by j)
+  int **firstneigh, /*list->firstneigh*/
+  //firsttouch[i][jj] = 1, if i and j are in contact
+  //                    0, otherwise
+  int **firsttouch, /*listgranhistory->firstneigh*/
+  //firstshear[i][3*jj] is the shear vector between atoms i and j
+  double **firstshear /*listgranhistory->firstdouble*/) {
+
+  //shearmap[i] is a hashmap for atoms in contact with atom i
+  struct hashmap **shearmap;
+  shearmap = (struct hashmap **)malloc(sizeof(struct hashmap) * inum);
+
+  for (int ii=0; ii<inum; ii++) {
+    int i = ilist[ii];
+    int jnum = numneigh[i];
+    assert(jnum < 16);
+
+    struct hashmap *hm = create_hashmap(16);
+    for (int jj = 0; jj<jnum; jj++) {
+      int j = firstneigh[i][jj];
+
+      //TODO: necessary to check firsttouch[i][jj] == 1?
+      double *shear = &firstshear[i][3*jj];
+      insert_hashmap(hm, j, shear);
+    }
+    shearmap[i] = hm;
+  }
+
+  return shearmap;
 }
 
-EXTERN double hertz_gpu_cell()
+struct hashmap *c_to_device_shearmap(struct hashmap **shearmap, int inum) {
+  struct hashmap *d_shearmap;
+  ASSERT_NO_CUDA_ERROR(
+    cudaMalloc((void **)&d_shearmap, sizeof(struct hashmap) * inum));
+
+  for (int i=0; i<inum; i++) {
+    struct hashmap *d_hm = c_to_device_hashmap(shearmap[i]);
+    ASSERT_NO_CUDA_ERROR(cudaMemcpy(&d_shearmap[i], d_hm, sizeof(struct hashmap), cudaMemcpyDeviceToDevice));
+  }
+  return d_shearmap;
+}
+
+EXTERN double hertz_gpu_cell(
+  const bool eflag, const bool vflag,
+  const int inum, const int nall, const int ago,
+
+  //inputs
+  double **host_x, double **host_v, double **host_omega,
+  double *host_radius, double *host_rmass,
+  int *host_type,
+  double dt,
+
+  //stiffness parameters
+  int num_atom_types,
+  double **host_Yeff,
+  double **host_Geff,
+  double **host_betaeff,
+  double **host_coeffFrict,
+  double nktv2p,
+
+  //inouts
+  struct hashmap**host_shear, double **host_torque, double **host_force)
 {
+  static int blockSize = BLOCK_1D;
+  static int ncell = ncellx*ncelly*ncellz;
+
+  // -----------------------------------------------------------
+  // Device variables
+  // -----------------------------------------------------------
+  const int SIZE_1D = (nall * sizeof(double));
+  const int SIZE_2D = (3 * SIZE_1D);
+  static bool first_call = true;
+  if (first_call) {
+    first_call = false;
+
+    init_cell_list(cell_list_gpu, nall, ncell, blockSize);
+
+    //malloc device data
+    cudaMalloc((void**)&d_v, SIZE_2D);
+    cudaMalloc((void**)&d_omega, SIZE_2D);
+    cudaMalloc((void**)&d_shear, SIZE_2D);
+    cudaMalloc((void**)&d_torque, SIZE_2D);
+    cudaMalloc((void**)&d_f, SIZE_2D);
+    //shear done later
+
+    cudaMalloc((void**)&d_radius, SIZE_1D);
+    cudaMalloc((void**)&d_rmass, SIZE_1D);
+
+    const int PARAM_SIZE = num_atom_types * num_atom_types * sizeof(double);
+    cudaMalloc((void**)&d_Yeff, PARAM_SIZE);
+    cudaMalloc((void**)&d_Geff, PARAM_SIZE);
+    cudaMalloc((void**)&d_betaeff, PARAM_SIZE);
+    cudaMalloc((void**)&d_coeffFrict, PARAM_SIZE);
+
+    //flatten stiffness lookup tables
+    double *h_Yeff = (double *)malloc(PARAM_SIZE);
+    double *h_Geff = (double *)malloc(PARAM_SIZE);
+    double *h_betaeff = (double *)malloc(PARAM_SIZE);
+    double *h_coeffFrict = (double *)malloc(PARAM_SIZE);
+    for (int i=0; i<num_atom_types; i++) {
+      for (int j=0; j<num_atom_types; j++) {
+        h_Yeff[i + (j*num_atom_types)] = host_Yeff[i][j];
+        h_Geff[i + (j*num_atom_types)] = host_Geff[i][j];
+        h_betaeff[i + (j*num_atom_types)] = host_betaeff[i][j];
+        h_coeffFrict[i + (j*num_atom_types)] = host_coeffFrict[i][j];
+      }
+    }
+    cudaMemcpy(d_Yeff, h_Yeff, PARAM_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Geff, h_Geff, PARAM_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_betaeff, h_betaeff, PARAM_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_coeffFrict, h_coeffFrict, PARAM_SIZE, cudaMemcpyHostToDevice);
+  }
+
+  //flatten 2d atom data
+  double *h_v = (double *)malloc(SIZE_2D);
+  double *h_omega = (double *)malloc(SIZE_2D);
+  double *h_torque = (double *)malloc(SIZE_2D);
+  double *h_f = (double *)malloc(SIZE_2D);
+  for (int i=0; i< nall; i++) {
+    for (int j=0; j<3; j++) {
+      h_v[(i*3)+j] = host_v[i][j];
+      h_omega[(i*3)+j] = host_omega[i][j];
+      h_torque[(i*3)+j] = host_torque[i][j];
+      h_f[(i*3)+j] = host_force[i][j];
+    }
+  }
+  cudaMemcpy(d_v, h_v, SIZE_2D, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_omega, h_omega, SIZE_2D, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_torque, h_torque, SIZE_2D, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_f, h_f, SIZE_2D, cudaMemcpyHostToDevice);
+
+  //just copy across 1d atom data
+  cudaMemcpy(d_radius, host_radius, SIZE_1D, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_rmass, host_rmass, SIZE_1D, cudaMemcpyHostToDevice);
+
+  build_cell_list(host_x[0], host_type, cell_list_gpu,
+	  ncell, ncellx, ncelly, ncellz, blockSize, inum, nall, ago);
+
+  struct hashmap *d_shearmap = c_to_device_shearmap(host_shear, inum);
+
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    printf("Pre-kernel error: %s.\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
+  const int BX=blockSize;
+  dim3 GX(ncellx, ncelly*ncellz);
+  kernel_hertz_cell<true,true,64><<<GX,BX>>>(
+    cell_list_gpu.pos,
+    cell_list_gpu.idx,
+    cell_list_gpu.type,
+    cell_list_gpu.natom,
+    inum, ncellx, ncelly, ncellz,
+    d_v, d_omega, d_radius, d_rmass, d_shearmap, d_torque, d_f,
+    dt, num_atom_types,
+    d_Yeff, d_Geff, d_betaeff, d_coeffFrict, nktv2p
+  );
+
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    printf("Post-kernel error: %s.\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
+  //copy back force calculations (shear, torque, force)
+//cudaMemcpy(h_shear, d_shear, SIZE_2D, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_torque, d_torque, SIZE_2D, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_f, d_f, SIZE_2D, cudaMemcpyDeviceToHost);
+  for (int i=0; i<nall; i++) {
+    host_torque[i][0] = h_torque[(i*3)];
+    host_torque[i][1] = h_torque[(i*3)+1];
+    host_torque[i][2] = h_torque[(i*3)+2];
+
+    host_force[i][0] = h_f[(i*3)];
+    host_force[i][1] = h_f[(i*3)+1];
+    host_force[i][2] = h_f[(i*3)+2];
+  }
+
   return 0.0;
 }
 
--- lib/gpu/hertz_gpu_kernel.h
+++ lib/gpu/hertz_gpu_kernel.h
@@ -5,7 +5,7 @@
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under 
+   certain rights in this software.  This software is distributed under
    the GNU General Public License.
 
    See the README file in the top-level LAMMPS directory.
@@ -20,9 +20,197 @@
 #ifndef HERTZ_GPU_KERNEL
 #define HERTZ_GPU_KERNEL
 
+#include "hashmap.cu"
+#define sqrtFiveOverSix 0.91287092917527685576161630466800355658790782499663875
+
+__device__ void pair_interaction(
+  //inputs
+    double *xi, double *xj,           //position
+    double *vi, double *vj,           //velocity
+    double *omegai, double *omegaj,   //rotational velocity
+    double radi, double radj,         //radius
+    double rmassi, double rmassj,     //] mass
+    double massi, double massj,       //]
+    int typei, int typej,             //type
+    double dt,                        //timestep
+
+  //contact model parameters inputs
+    int num_atom_types,
+    double *Yeff,
+    double *Geff,
+    double *betaeff,
+    double *coeffFrict,
+    double nktv2p,
+
+  //inouts
+    double *shear,
+    double *torque,
+    double *force) {
+
+  // del is the vector from j to i
+  double delx = xi[0] - xj[0];
+  double dely = xi[1] - xj[1];
+  double delz = xi[2] - xj[2];
+
+  double rsq = delx*delx + dely*dely + delz*delz;
+  double radsum = radi + radj;
+  if (rsq >= radsum*radsum) {
+    //unset non-touching atoms
+    shear[0] = 0.0;
+    shear[1] = 0.0;
+    shear[2] = 0.0;
+  } else {
+    //distance between centres of atoms i and j
+    //or, magnitude of del vector
+    double r = sqrt(rsq);
+    double rinv = 1.0/r;
+    double rsqinv = 1.0/rsq;
+
+    // relative translational velocity
+    double vr1 = vi[0] - vj[0];
+    double vr2 = vi[1] - vj[1];
+    double vr3 = vi[2] - vj[2];
+
+    // normal component
+    double vnnr = vr1*delx + vr2*dely + vr3*delz;
+    double vn1 = delx*vnnr * rsqinv;
+    double vn2 = dely*vnnr * rsqinv;
+    double vn3 = delz*vnnr * rsqinv;
+
+    // tangential component
+    double vt1 = vr1 - vn1;
+    double vt2 = vr2 - vn2;
+    double vt3 = vr3 - vn3;
+
+    // relative rotational velocity
+    double wr1 = (radi*omegai[0] + radj*omegaj[0]) * rinv;
+    double wr2 = (radi*omegai[1] + radj*omegaj[1]) * rinv;
+    double wr3 = (radi*omegai[2] + radj*omegaj[2]) * rinv;
+
+    // normal forces = Hookian contact + normal velocity damping
+    double mi,mj;
+    if (rmassi && rmassj) {
+      mi=rmassi;
+      mj=rmassj;
+    } else if (massi && massj) {
+      mi=massi;
+      mj=massj;
+    } else {
+      //this should never fire
+      return;
+    }
+    double meff = mi*mj/(mi+mj);
+    //not-implemented: freeze_group_bit
+
+    double deltan = radsum-r;
+
+    //derive contact model parameters (inlined)
+    //Yeff, Geff, betaeff, coeffFrict are lookup tables
+    //TODO: put these in shared memory
+    int typeij = typei + (typej * num_atom_types);
+    double reff = radi * radj / (radi + radj);
+    double sqrtval = sqrt(reff * deltan);
+    double Sn = 2.    * Yeff[typeij] * sqrtval;
+    double St = 8.    * Geff[typeij] * sqrtval;
+    double kn = 4./3. * Yeff[typeij] * sqrtval;
+    double kt = St;
+    double gamman=-2.*sqrtFiveOverSix*betaeff[typeij]*sqrt(Sn*meff);
+    double gammat=-2.*sqrtFiveOverSix*betaeff[typeij]*sqrt(St*meff);
+    double xmu=coeffFrict[typeij];
+    //not-implemented if (dampflag == 0) gammat = 0;
+    kn /= nktv2p;
+    kt /= nktv2p;
+
+    double damp = gamman*vnnr*rsqinv;
+	  double ccel = kn*(radsum-r)*rinv - damp;
+
+    //not-implemented cohesionflag
+
+    // relative velocities
+    double vtr1 = vt1 - (delz*wr2-dely*wr3);
+    double vtr2 = vt2 - (delx*wr3-delz*wr1);
+    double vtr3 = vt3 - (dely*wr1-delx*wr2);
+
+    // shear history effects
+    shear[0] += vtr1 * dt;
+    shear[1] += vtr2 * dt;
+    shear[2] += vtr3 * dt;
+
+    // rotate shear displacements
+    double rsht = shear[0]*delx + shear[1]*dely + shear[2]*delz;
+    rsht *= rsqinv;
+
+    shear[0] -= rsht*delx;
+    shear[1] -= rsht*dely;
+    shear[2] -= rsht*delz;
+
+    // tangential forces = shear + tangential velocity damping
+    double fs1 = - (kt*shear[0] + gammat*vtr1);
+    double fs2 = - (kt*shear[1] + gammat*vtr2);
+    double fs3 = - (kt*shear[2] + gammat*vtr3);
+
+    // rescale frictional displacements and forces if needed
+    double fs = sqrt(fs1*fs1 + fs2*fs2 + fs3*fs3);
+    double fn = xmu * fabs(ccel*r);
+    double shrmag = 0;
+    if (fs > fn) {
+      shrmag = sqrt(shear[0]*shear[0] +
+                    shear[1]*shear[1] +
+                    shear[2]*shear[2]);
+      if (shrmag != 0.0) {
+        shear[0] = (fn/fs) * (shear[0] + gammat*vtr1/kt) - gammat*vtr1/kt;
+        shear[1] = (fn/fs) * (shear[1] + gammat*vtr2/kt) - gammat*vtr2/kt;
+        shear[2] = (fn/fs) * (shear[2] + gammat*vtr3/kt) - gammat*vtr3/kt;
+        fs1 *= fn/fs;
+        fs2 *= fn/fs;
+        fs3 *= fn/fs;
+      } else {
+        fs1 = fs2 = fs3 = 0.0;
+      }
+    }
+
+    double fx = delx*ccel + fs1;
+    double fy = dely*ccel + fs2;
+    double fz = delz*ccel + fs3;
+
+    double tor1 = rinv * (dely*fs3 - delz*fs2);
+    double tor2 = rinv * (delz*fs1 - delx*fs3);
+    double tor3 = rinv * (delx*fs2 - dely*fs1);
+
+    // this is what we've been working up to!
+    force[0] += fx;
+    force[1] += fy;
+    force[2] += fz;
+
+    torque[0] -= radi*tor1;
+    torque[1] -= radi*tor2;
+    torque[2] -= radi*tor3;
+  }
+}
+
 /* Cell list version of hertz kernel */
 template<bool eflag, bool vflag, int blockSize>
-__global__ void kernel_hertz_cell()
+__global__ void kernel_hertz_cell(
+			       float3 *cell_list, unsigned int *cell_idx,
+			       int *cell_type, int *cell_atom,
+			       const int inum,
+			       const int ncellx, const int ncelly, const int ncellz,
+             //passed in global memory
+             double *v,
+             double *omega,
+             double *radius,
+             double *rmass,
+             struct hashmap *shearmap,  //] inouts
+             double *torque,            //]
+             double *f,                 //]
+             //passed through value
+             double dt,
+             int num_atom_types,
+             double *Yeff,
+             double *Geff,
+             double *betaeff,
+             double *coeffFrict,
+             double nktv2p)
 {
   // calculate 3D block idx from 2d block
   int bx = blockIdx.x;
@@ -30,9 +218,141 @@ __global__ void kernel_hertz_cell()
   int bz = blockIdx.y / ncelly;
 
   int tid = threadIdx.x;
-  
+
   // compute cell idx from 3D block idx
   int cid = bx + INT_MUL(by, ncellx) + INT_MUL(bz, INT_MUL(ncellx,ncelly));
+
+  __shared__ int typeSh[blockSize];
+  __shared__ float posSh[blockSize*3];
+
+  // compute neighbour cell bounds
+  int nborz0 = max(bz-1,0), nborz1 = min(bz+1, ncellz-1),
+      nbory0 = max(by-1,0), nbory1 = min(by+1, ncelly-1),
+      nborx0 = max(bx-1,0), nborx1 = min(bx+1, ncellx-1);
+
+  // for every atom in the cell (modulo current threadid)
+  for (int ii = 0; ii < ceil((float)(cell_atom[cid])/blockSize); ii++) {
+    int i = tid + ii*blockSize;
+    unsigned int answer_pos = cell_idx[cid*blockSize+i];
+
+    // load current cell atom position and type into sMem
+    for (int j = tid; j < cell_atom[cid]; j += blockSize) {
+      int pid = cid*blockSize + j;
+      float3 pos = cell_list[pid];
+      posSh[j            ] = pos.x;
+      posSh[j+  blockSize] = pos.y;
+      posSh[j+2*blockSize] = pos.z;
+      typeSh[j]            = cell_type[pid];
+    }
+    __syncthreads();
+    if (answer_pos < inum) {
+      double xi[3]; double xj[3];
+      double vi[3]; double vj[3];
+      double omegai[3]; double omegaj[3];
+      double radi; double radj;
+      double rmassi; double rmassj;
+      int typei; int typej;
+      double *shear;
+      double *torquei; double *force;
+
+      xi[0] = posSh[i            ];
+      xi[1] = posSh[i+  blockSize];
+      xi[2] = posSh[i+2*blockSize];
+      typei = typeSh[i];
+      //load from global memory (TODO: shift to shared)
+      vi[0] = v[cell_idx[i]];
+      vi[1] = v[cell_idx[i]+1];
+      vi[2] = v[cell_idx[i]+2];
+      omegai[0] = omega[cell_idx[i]];
+      omegai[1] = omega[cell_idx[i]+1];
+      omegai[2] = omega[cell_idx[i]+2];
+      radi = radius[cell_idx[i]];
+      rmassi = rmass[cell_idx[i]];
+      force = &f[cell_idx[i]];
+      torquei = &torque[cell_idx[i]];
+
+      // compute force within cell first
+      for (int j = 0; j < cell_atom[cid]; j++) {
+	      if (j == i) continue;
+        xj[0] = posSh[j            ];
+        xj[1] = posSh[j+  blockSize];
+        xj[2] = posSh[j+2*blockSize];
+        typej = typeSh[j];
+        vj[0] = v[cell_idx[j]];
+        vj[1] = v[cell_idx[j]+1];
+        vj[2] = v[cell_idx[j]+2];
+        omegaj[0] = omega[cell_idx[j]];
+        omegaj[1] = omega[cell_idx[j]+1];
+        omegaj[2] = omega[cell_idx[j]+2];
+        radj = radius[cell_idx[j]];
+        rmassj = rmass[cell_idx[j]];
+        struct entry *lookup = cuda_retrieve_hashmap(&shearmap[i], j);
+        if (lookup == NULL) {
+          //complain bitterly and refuse to continue
+        }
+        shear = lookup->shear;
+
+        pair_interaction(
+            xi, xj, vi, vj, omegai, omegaj,
+            radi, radj, rmassi, rmassj, 0, 0, typei, typej,
+            //passed through (constant)
+            dt, num_atom_types, Yeff, Geff, betaeff, coeffFrict, nktv2p,
+            shear, torquei, force);
+      }
+      __syncthreads();
+
+      // compute force from neigboring cells
+      for (int nborz = nborz0; nborz <= nborz1; nborz++) {
+        for (int nbory = nbory0; nbory <= nbory1; nbory++) {
+          for (int nborx = nborx0; nborx <= nborx1; nborx++) {
+            if (nborz == bz && nbory == by && nborx == bx) continue;
+	          int cid_nbor = nborx +
+                           INT_MUL(nbory,ncellx) +
+	                         INT_MUL(nborz,INT_MUL(ncellx,ncelly));
+
+            // load neighbor cell position and type into smem
+            for (int j = tid; j < cell_atom[cid_nbor]; j += blockSize) {
+              int pid = INT_MUL(cid_nbor,blockSize) + j;
+              float3 pos = cell_list[pid];
+              posSh[j            ] = pos.x;
+              posSh[j+  blockSize] = pos.y;
+              posSh[j+2*blockSize] = pos.z;
+              typeSh[j]           = cell_type[pid];
+            }
+            __syncthreads();
+            if (answer_pos < inum) {
+              for (int j = 0; j < cell_atom[cid_nbor]; j++) {
+                xj[0] = posSh[j            ];
+                xj[1] = posSh[j+  blockSize];
+                xj[2] = posSh[j+2*blockSize];
+                typej = typeSh[j];
+                vj[0] = v[cell_idx[j]];
+                vj[1] = v[cell_idx[j]+1];
+                vj[2] = v[cell_idx[j]+2];
+                omegaj[0] = omega[cell_idx[j]];
+                omegaj[1] = omega[cell_idx[j]+1];
+                omegaj[2] = omega[cell_idx[j]+2];
+                radj = radius[cell_idx[j]];
+                rmassj = rmass[cell_idx[j]];
+                struct entry *lookup = cuda_retrieve_hashmap(&shearmap[i], j);
+                if (lookup == NULL) {
+                  //complain bitterly and refuse to continue
+                }
+                shear = lookup->shear;
+
+                pair_interaction(
+                    xi, xj, vi, vj, omegai, omegaj,
+                    radi, radj, rmassi, rmassj, 0, 0, typei, typej,
+                    //passed through (constant)
+                    dt, num_atom_types, Yeff, Geff, betaeff, coeffFrict, nktv2p,
+                    shear, torquei, force);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 #endif
